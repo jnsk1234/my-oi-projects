dd727c5c1f3023d937e7b7e8f34fcb8d12ed26a4
jnsk1234 <18765917859@163.com>
2026-06-01 22:41:22 +0800

    模版

--- Untitled-2.cpp
+++ Untitled-2.cpp
@@ -1,52 +1,28 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
+// 输入输出优化
+void fastIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+// 类型定义
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+// 常量定义
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;
 const int MOD = 1e9 + 7;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    fastIO();
     
-    int n;
-    cin >> n;
-    
-    vector<vector<int>> g(n, vector<int>(n));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> g[i][j];
-        }
-    }
-    
-    int fullmask = (1 << n) - 1;
-    vector<vector<int>> dp(1 << n, vector<int>(n, 0));
-    dp[1][0] = 1;  // 固定起点为0
-    
-    for (int mask = 0; mask < (1 << n); mask++) {
-        for (int last = 0; last < n; last++) {
-            if (!dp[mask][last]) continue;
-            
-            // 如果还没排满所有人，继续安排下一个
-            if (mask != fullmask) {
-                for (int nxt = 0; nxt < n; nxt++) {
-                    if ((mask >> nxt) & 1) continue;  // 已经排过了
-                    if (g[last][nxt] == 1) {
-                        dp[mask | (1 << nxt)][nxt] = 
-                            (dp[mask | (1 << nxt)][nxt] + dp[mask][last]) % MOD;
-                    }
-                }
-            }
-        }
-    }
-    
-    int ans = 0;
-    // 检查所有排满的情况，看最后一个人能否回到起点0
-    for (int last = 0; last < n; last++) {
-        if (g[last][0] == 1) {
-            ans = (ans + dp[fullmask][last]) % MOD;
-        }
-    }
-    
-    cout << ans << endl;
+    // 你的代码在这里
     
     return 0;
 }
